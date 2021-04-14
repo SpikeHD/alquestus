195ef3d31a6d925329830cb3ce75681efbe1cc49
spikegd <spikegdofficial@gmail.com>
2021-04-14 15:57:49 -0700

    Remove old menu stuff

--- game/menu.js
+++ game/menu.js
@@ -13,16 +13,6 @@ const rows = process.stdout.rows
 debug('Columns: ' + columns)
 debug('Rows: ' + rows)
 
-// Calculate placing the menu items equally at the bottom of the screen
-let itemSpace = 2
-let totalItemSpace = buttons.join(Array(itemSpace).fill(' ').join()).length
-debug(totalItemSpace)
-
-if (totalItemSpace + columns % 2 === 0) totalItemSpace--
-
-debug(itemSpace)
-debug(totalItemSpace)
-
 module.exports = async () => {
   menuDraw()
 
@@ -32,30 +22,5 @@ module.exports = async () => {
 }
 
 function menuDraw() {
-  for(let i = 0; i < rows; i++) {
-    if (i !== rows-1) {
-      draw(chalk.blue(Array(columns).fill('█').join('')))
-      continue
-    }
 
-    // Fill first half
-    let row = ''
-    const totalBlank = columns - totalItemSpace
-    const space = Array(totalBlank/2).fill('█').join('')
-
-    // Button highlight handling
-    let textBtns = ''
-
-    buttons.forEach((b, i) => {
-      if (i === selected) textBtns += chalk.white(chalk.bgRed(b))
-      else textBtns += b
-
-      // Item spacing
-      if (i !== buttons.length-1) textBtns += Array(itemSpace).fill('-').join('')
-    })
-
-    row += chalk.blue(space + textBtns + space)
-
-    draw(row)
-  }
 }
