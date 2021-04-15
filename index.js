c18ceec36e642f2dd0c061d8f173116f3506d2ca
spikegd <spikegdofficial@gmail.com>
2021-04-14 21:55:29 -0700

    Game starting

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-process.stdin.setRawMode(true)
 const menu = require('./game/menu')
+const main = require('./game/main')
 
 ;(async () => {
+  // First show the menu
   await menu()
+
+  // Once the menu is returned, we start the game!
+  await main()
 })()
