21dffe06c2bbe87f49312e52c9d2d48c5b704b4b
SpikeHD <spikegdofficial@gmail.com>
2021-04-18 00:33:33 -0700

    attempt finising second half

--- game/generation/corridor.js
+++ game/generation/corridor.js
@@ -35,21 +35,20 @@ function generateWalls(first, second, holes) {
     const axis = fSide.angle === 'horiz' ? 'y':'x'
     // YES THIS NEEDS TO BE FLIPPED TOO BITCH
     const drawAngle = fSide.angle === 'horiz' ? 'vert':'horiz'
-    // NOT TO SELF: DONT HARDCODE AS length OR ELSE I WONT BE ABLE TO DO VERT WALL ENTRANCES
-    let length = holes[1].y - holes[0].y
+    let length = holes[1][axis === 'x' ? 'y':'x'] - holes[0][axis === 'x' ? 'y':'x']
     let size = Math.round((holes[1][axis] - holes[0][axis])/2)
 
     // Push the first half of the wall, which ends where the vertical one will begin
     walls.push({
       angle: drawAngle,
-      size: size,
+      size,
       start: { x: holes[0].x + (axis === 'x' ? 0:1), y: holes[0].y + (axis === 'y' ? 0:1) }
     })
 
     // Other side, yknow, since it's a hallway
     walls.push({
       angle: drawAngle,
-      size: size,
+      size,
       start: { x: holes[0].x - (axis === 'x' ? 0:1), y: holes[0].y - (axis === 'y' ? 0:1) }
     })
 
@@ -60,6 +59,7 @@ function generateWalls(first, second, holes) {
       const closerWall = walls.reduce((prev, cur) => (Math.abs(cur[axis] - holes[1][axis]) < Math.abs(prev[axis] - holes[1][axis])) ? cur : prev)
       let startX = closerWall.start.x < holes[1].x ? closerWall.start.x + closerWall.size : closerWall.start.x 
       let startY = closerWall.start.y < holes[1].y ? closerWall.start.y + closerWall.size : closerWall.start.y
+      let wasNegative = false
 
       // Add an extra wall to the farther wall to create a proper corner
       walls[walls.indexOf(walls.find(w => w !== closerWall))].size += 2
@@ -67,8 +67,8 @@ function generateWalls(first, second, holes) {
       // Negative length? Draw from end to start instead
       if (length < 0) {
         length = -length
-
         startY = startY-length
+        wasNegative = true
       }
 
       // Closer wall
@@ -81,10 +81,26 @@ function generateWalls(first, second, holes) {
       // Farther wall
       walls.push({
         angle: fSide.angle,
-        size: length + 2,
-        start: { x: startX + (fSide.angle === 'horiz' ? 2:0), y: startY + (fSide.angle === 'vert' ? 2:0) }
+        size: length + (wasNegative ? -2:2),
+        start: { x: startX + (fSide.angle === 'horiz' ? 0:2), y: startY + (fSide.angle === 'vert' ? 0:2) }
       })
     }
+
+    const endX = size - holes[1].x
+    const endY = size - holes[1].y
+
+    // Last two walls
+    walls.push({
+      angle: drawAngle,
+      size,
+      start: { x: endX + (axis === 'x' ? 0:1), y: endY + (axis === 'y' ? 0:1) }
+    })
+
+    walls.push({
+      angle: drawAngle,
+      size,
+      start: { x: endX - (axis === 'x' ? 0:1), y: endY - (axis === 'y' ? 0:1) }
+    })
   }
 
   return walls
